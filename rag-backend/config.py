851762538c9d9a0fc3c6af9b1b9aff4927b1cfbd
liamellison02 <liam.ellison.codes@gmail.com>
2024-08-08 19:21:48 -0400

    set port for flask server

--- rag-backend/config.py
+++ rag-backend/config.py
@@ -20,4 +20,4 @@ def index():
 
 if __name__ == '__main__':
     """Set debug to False for production"""
-    app.run(debug=True, use_reloader=True) 
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=5000) 
